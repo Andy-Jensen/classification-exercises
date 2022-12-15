5c916d4c07e8dfc6213fe8fa7136194772bea67a
Andy Jensen <andrew.j.jensen91@gmail.com>
2022-12-15 16:58:16 -0600

    updates

--- acquire.py
+++ acquire.py
@@ -1,12 +1,22 @@
+
+#import libraries and the 'get_connection' function from env
 import pandas as pd
 import numpy as np
 import env
 import os
 from env import get_connection
+
 def get_titanic():
+    '''
+    looking for an already existing titanic csv on the local machine
+    '''
     if os.path.isfile('titanic.csv'):
         return pd.read_csv('titanic.csv')
     else:
+        '''
+        if there is no existing csv, then connect to the SQL server and get the information from 
+        titanic_db
+        '''
         url = get_connection('titanic_db')
         
         query = '''
@@ -14,13 +24,25 @@ def get_titanic():
                 '''
         
         df = pd.read_sql(query, url)
+        '''
+        saving the newly queried SQL table to a csv so it
+        can be used instead of connecting to the SQL server
+        every time I want this info
+        '''
         df.to_csv('titanic.csv', index=False)
         return df
 
 def get_telco_data():
+    '''
+    looking for an already existing telco_churn csv on the local machine
+    '''
     if os.path.isfile('telco_churn.csv'):
         return pd.read_csv('telco_churn.csv')
     else:
+        '''
+        if there is no existing csv, then connect to the SQL server and get the information from 
+        telco_churn db
+        '''
         url = get_connection('telco_churn')
         
         query = '''
@@ -34,13 +56,25 @@ def get_telco_data():
                 '''
         
         df = pd.read_sql(query, url)
+        '''
+        saving the newly queried SQL table to a csv so it
+        can be used instead of connecting to the SQL server
+        every time I want this info
+        '''
         df.to_csv('telco_churn.csv', index=False)
         return df
 
 def get_iris_data():
+    '''
+    looking for an already existing iris csv on the local machine
+    '''
     if os.path.isfile('iris_db.csv'):
         return pd.read_csv('iris_db.csv')
     else:
+        '''
+        if there is no existing csv, then connect to the SQL server and get the information from 
+        iris_db
+        '''
         url = get_connection('iris_db')
         
         query = '''
@@ -48,6 +82,11 @@ def get_iris_data():
                 '''
         
         df = pd.read_sql(query, url)
+        '''
+        saving the newly queried SQL table to a csv so it
+        can be used instead of connecting to the SQL server
+        every time I want this info
+        '''
         df.to_csv('iris_db.csv', index=False)
         return df
 
--- prepare.py
+++ prepare.py
@@ -1,14 +1,22 @@
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 
-
+#function for doing train test split on any dataset
 def tts(df):
+    '''
+    assigning the column of the dataframe that you want to stratify by
+    '''
     x=input('stratify=')
+    '''
+    removing your test data from the data
+    '''
     train_validate, test=train_test_split(df, 
                                  train_size=.8, 
                                  random_state=8675309, 
                                  stratify=df[x])
-                
+    '''
+    splitting the remaining data into the train and validate groups
+    '''            
     train, validate =train_test_split(train_validate, 
                                       test_size=.3, 
                                       random_state=8675309, 
